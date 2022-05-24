0480b00577b4160684c43ccad95a11aa901807dc
Gustavo Ogliari <guga_ogliari@hotmail.com>
2022-05-24 20:12:46 -0300

    Initial commit

---
+++ src/test/java/calculadora/TestCalculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCalculadora {
+
+    @Test
+    public void testGetAdicao() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getAdicao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+}
